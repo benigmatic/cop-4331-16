2f535df4de1455d90bbe67bd5389f4e82e75e645
benigmatic <krivonos.alexandra25@gmail.com>
2021-07-27 00:32:50 -0400

    edititem2

--- api.js
+++ api.js
@@ -863,6 +863,7 @@ console.log("Error sending the email "+ err);
        //var ret='';
        var error = '';
        const { userId, Name, Brand, Model, Category, Location, Replacement, Serial, itemId, jwtToken } = req.body;
+       /*
         try
        {
          if( token.isExpired(jwtToken))
@@ -876,7 +877,7 @@ console.log("Error sending the email "+ err);
        {
          console.log(e.message);
        }
-
+*/
 
       // const filter = {userId: userId, itemId: itemId}
       // const newItem = {Name:Name, Brand:Brand, Model:Model, Category:Category, Location:Location, Replacement:Replacement, Serial:Serial};
@@ -894,7 +895,7 @@ console.log("Error sending the email "+ err);
 
         })
 
-       arr = await db.collection('Assets').find({userId:id}).toArray();
+      
        }
        catch(e){
 
@@ -911,7 +912,7 @@ console.log("Error sending the email "+ err);
          console.log(e.message);
        }
 
-       var ret = { error: error, jwtToken: refreshedToken, arr:arr};
+       var ret = { error: error, jwtToken: refreshedToken};
 
        res.status(200).json(ret);
      });
